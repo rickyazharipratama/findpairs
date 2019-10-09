f48771144b193b38b891dc65da231a20bad9d1a1
Ricky <ricky.pratama@mercatocapitale.com>
2019-10-09 21:37:14 +0700

    - add sound model - add setting action button widget - add widget observer life cycle in landing page

--- lib/Presenters/Pages/LandingPagePresenter.dart
+++ lib/Presenters/Pages/LandingPagePresenter.dart
@@ -1,14 +1,16 @@
 
 import 'dart:async';
 
+import 'package:assets_audio_player/assets_audio_player.dart';
+import 'package:findpairs/FindPairsApp.dart';
 import 'package:findpairs/PresenterViews/Pages/LandingPageView.dart';
 import 'package:findpairs/Presenters/Pages/BasePagePresenter.dart';
+import 'package:flutter/rendering.dart';
 
 class LandingPagePresenter extends BasePagePresenter{
 
   LandingPageView _view;
   StreamController<Map<String,dynamic>> _streamController;
-
 
   StreamController get streamController => _streamController;
 
@@ -30,6 +32,41 @@ class LandingPagePresenter extends BasePagePresenter{
   @override
   void initiateData() {
     super.initiateData();
+    FindPairsApp.of(view.currentContext()).presenter.bgSound.open(
+      AssetsAudio(
+        asset: "landing-page.ogg",
+        folder: "assets/sounds/"
+      )
+    );
+    FindPairsApp.of(view.currentContext()).presenter.bgSound.
+    FindPairsApp.of(view.currentContext()).presenter.bgSound.finished.listen((isFinished){
+      debugPrint("music finish listen called");
+       if(isFinished){
+         debugPrint("music isFinished");
+         stopBackgroundMusic();
+         Timer(const Duration(milliseconds: 1500),(){
+           playBackgroundMusic();
+         });
+       }
+    });
+    playBackgroundMusic();
+  }
 
+  @override
+  playBackgroundMusic(){
+    super.playBackgroundMusic();
+    FindPairsApp.of(view.currentContext()).presenter.bgSound.play();
+  }
+
+  @override
+  pauseBackgroundMusic() {
+    super.pauseBackgroundMusic();
+    FindPairsApp.of(view.currentContext()).presenter.bgSound.pause();
+  }
+  
+  @override
+  stopBackgroundMusic() {
+    super.stopBackgroundMusic();
+    FindPairsApp.of(view.currentContext()).presenter.bgSound.stop();
   }
 }
